b5b9bc8b1a192f79c9610ad593df73425b50788a
blanche <blanche@users.noreply.github.com>
2016-12-28 11:51:25 +0100

    implement fullpage.js website to create rough structure of the page and include graphs in placeholders

---
+++ src/charts-js/retailing.js
@@ -0,0 +1,43 @@
+// from http://bl.ocks.org/phil-pedruco/7243857
+d3.csv('./data2.csv', function (error, data) {
+    console.log(data);
+    if (error) {
+        console.error(error);
+    }
+    // create an empty object that nv is expecting
+    retailing_data = [
+        {
+            key: "EUR",
+            type: "line",
+            yAxis: 1,
+            values: []
+        },
+        {
+            key: "Retailing",
+            type: "line",
+            yAxis: 2,
+            values: []
+        }
+    ];
+    data.forEach(function (d) {
+        retailing_data[1].values.push({x: Date.parse(d.Date), y: parseFloat(d.EUR)});
+        retailing_data[0].values.push({x: Date.parse(d.Date), y: parseFloat(d.AllRetailingExcludingAutomotiveFuel)});
+    });
+
+    nv.addGraph(function () {
+        var chart = nv.models.multiChart()
+            .margin({top: 30, right: 60, bottom: 50, left: 70})
+            .color(d3.scale.category10().range());
+        chart.xAxis.tickFormat(function (d) {
+            return d3.time.format('%b %d %Y')(new Date(d))
+        });
+        chart.yAxis1.tickFormat(d3.format(',.1f'));
+        chart.yAxis2.tickFormat(d3.format(',.1f'));
+
+        d3.select('#retailing svg')
+            .datum(retailing_data)
+            .transition().duration(500).call(chart);
+
+        return chart;
+    });
+});
