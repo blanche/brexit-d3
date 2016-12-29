69dc3e4dea38bd67232e8504d5957befceb2784a
alokgogate <alokgogate@gmail.com>
2016-12-29 14:45:29 +0000

    Update imports.js

--- src/charts-js/imports.js
+++ src/charts-js/imports.js
@@ -31,13 +31,12 @@ d3.csv('./data2.csv', function (error, data) {
         chart.xAxis.tickFormat(function (d) {
             return d3.time.format('%b %d %Y')(new Date(d))
         });
-        chart.yAxis1.tickFormat(d3.format(',.1f'));
-        chart.yAxis2.tickFormat(d3.format(',.1f'));
+        chart.yAxis1.tickFormat(d3.format(',.2f'));
+        chart.yAxis2.tickFormat(d3.format(',.2f'));
 
         d3.select('#imports svg')
             .datum(imports_data)
             .transition().duration(500).call(chart);
-
         return chart;
     });
 });
