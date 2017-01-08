46ee817180f4140c4f16f01cc14611c45a1d81c0
alokgogate <alokgogate@gmail.com>
2017-01-08 17:02:02 +0000

    Upload new file

---
+++ src/charts-js/dailyftse-eur.js
@@ -0,0 +1,51 @@
+d3.csv('./data-dailyftse.csv', function (error, data) {    
+        console.log(data);
+        if (error) {
+            console.error(error);
+        }
+        // create an empty object that nv is expecting
+        dailyftse_eurdata = [
+            {
+                key: "Daily FTSE",
+                type: "line",
+                yAxis: 1,
+                values: []
+            },
+            {
+                key: "EUR",
+                type: "line",
+                yAxis: 2,
+                values: []
+            }
+        ];
+        data.forEach(function (d) {
+            dailyftse_eurdata[0].values.push({x: Date.parse(d.Date), y: parseFloat(d.FTSEDaily)});
+            dailyftse_eurdata[1].values.push({x: Date.parse(d.Date), y: parseFloat(d.GBPtoEUR)});
+			
+		});
+
+        nv.addGraph(function () {
+			var chart = nv.models.multiChart()
+                .margin({top: 30, right: 60, bottom: 50, left: 70})
+				//.yDomain1([1.12, 1.42])
+				//.xDomain([2015-01-01, 2016-10-01])
+                .color(d3.scale.category10().range());
+            
+			chart.xAxis.tickFormat(function (d) {
+                return d3.time.format('%b %d %Y')(new Date(d))
+            });
+			
+            //chart.yAxis1.tickFormat(d3.format(',.0f'));
+            chart.yAxis1.tickFormat(function(d) { return '£' + d3.format(',f')(d) });
+			chart.yAxis2.tickFormat(d3.format(',.2f'));
+			
+			chart.yAxis1.axisLabel('FTSE 100');
+			chart.yAxis2.axisLabel('EUR to GBP');
+			
+            d3.select('#dailyftse-eur svg')
+                .datum(dailyftse_eurdata)
+                .transition().duration(500).call(chart);
+
+            return chart;
+        });
+    });
