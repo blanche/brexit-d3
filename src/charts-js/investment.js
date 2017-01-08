188b18d34916aae85f3e59fe8e40daa514d589e9
alokgogate <alokgogate@gmail.com>
2017-01-08 17:03:36 +0000

    Upload new file

---
+++ src/charts-js/investment.js
@@ -0,0 +1,48 @@
+d3.csv('./data.csv', function (error, data) {    
+        console.log(data);
+        if (error) {
+            console.error(error);
+        }
+        // create an empty object that nv is expecting
+        invdata = [
+            {
+                key: "EUR",
+                type: "line",
+                yAxis: 1,
+                values: []
+            },
+			{
+                key: "Investment",
+                type: "bar",
+                yAxis: 2,
+                values: []
+            }
+        ];
+        data.forEach(function (d) {
+            invdata[0].values.push({x: Date.parse(d.Date), y: parseFloat(d.EUR)});
+            invdata[1].values.push({x: Date.parse(d.Date), y: parseFloat(d.investment_intentions)});
+
+		});
+
+        nv.addGraph(function () {
+			var chart = nv.models.multiChart()
+                .margin({top: 30, right: 60, bottom: 50, left: 70})
+				.yDomain1([0, 1.9])
+                .color(d3.scale.category10().range());
+            
+			chart.xAxis.tickFormat(function (d) {
+                return d3.time.format('%b %d %Y')(new Date(d))
+            });
+			
+            chart.yAxis1.tickFormat(d3.format(',.2f'));
+            chart.yAxis2.tickFormat(d3.format(',.2f'));
+			chart.yAxis1.axisLabel('EUR to GBP');
+			//chart.yAxis2.axisLabel('');
+			
+            d3.select('#investment svg')
+                .datum(invdata)
+                .transition().duration(500).call(chart);
+
+            return chart;
+        });
+    });
